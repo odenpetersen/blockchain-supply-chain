be5e523917ca7498e8ce6fb034b0f60fd7c1d2f5
Qthulhu42 <34537274+Qthulhu42@users.noreply.github.com>
2023-07-22 13:45:11 +1000

    Set up truffle, ganache, npm

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost
+      port: 8545,            // Ganache/Ganache-cli default port
+      network_id: "*"        // Any network ID
+    },
+    sepolia: {
+      host: "",
+      port: 8545,
+      network_id: "",
+      from: "",
+      gas: 6000000,          // Adjust this value based on your contract's gas requirements
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",     // Solidity compiler version
+    },
+  },
+};
+
